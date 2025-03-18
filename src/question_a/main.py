34b338404b4db50ae5effef7a1e599d9c9203d16
denzam05 <dennis.zambrano2@g.austincc.edu>
2025-03-18 00:59:47 +0000

    midterm final

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,17 @@
 #add import
+
+from question_a import get_miles_per_hour
+
+def main ():
+    
+    kilometers= int (input ("enter the number of kilometers"))
+    minutes= int (input ("enter the number of minutes"))
+
+    result= get_miles_per_hour (kilometers, minutes)
+
+    if result== 'invalid argument':
+        print(result)
+    else:
+        print( "the speed is {result:.6} miles per hour")
+
+main()     
--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -2,15 +2,15 @@
 import unittest
 
 #follow this example to add questions b, c, and d for testing including their functions
-from src.question_a.question_a import test_config
+
+from src.question_a.question_a import get_miles_per_hour
 from src.question_b.question_b import get_random_number
 from src.question_d.question_d import get_day_of_the_week
 from src.question_c.question_c import get_bonus_pay_amount
 class Test_Config(unittest.TestCase):
-
-    def test_question_a_config(self):
-        self.assertEqual(True, test_config())
-
+     def test_question_a_config (self):
+    
+         self.assertEqual(True, Test_Config ())
 def test_get_random_number(self):
          self.assertEqual(get_random_number() >= 1, True)
          self.assertEqual(get_random_number() <= 5, True)
@@ -26,10 +26,13 @@ def test_get_bonus_pay_amount(self):
  
 
 
-def test_get_day_of_week(self): # test for question_d
+def test_get_day_of_week(self):
          self.assertEqual (get_day_of_the_week(0), 'Invalid number')
          self.assertEqual (get_day_of_the_week(1), 'Monday')
          self.assertEqual (get_day_of_the_week(2), 'Tuesday')
          self.assertEqual (get_day_of_the_week(3), 'Wednesday')
          self.assertEqual (get_day_of_the_week(8), 'Invalid number')
  
+def test_get_miles_per_hour(self):
+        self.assertEqual (get_miles_per_hour (32,60), 19.883872)
+       
